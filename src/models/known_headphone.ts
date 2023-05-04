00151895c83e8f035b2bc2daa625f6ffd3e18ae6
Richard Race <1273965+richrace@users.noreply.github.com>
2023-05-04 17:06:37 +0100

    Added product IDs (don't know if they work)

--- src/models/known_headphone.ts
+++ src/models/known_headphone.ts
@@ -8,12 +8,15 @@ export default class KnownHeadphone {
   readonly writeBytes: number[];
 
   public static ArctisVendorID = 4152;
+
   public static Arctis7_2019_ProductID = 0x12ad;
   public static Arctis7_2017_ProductID = 0x1260;
   public static ArctisProProductID = 0x1252;
   public static Arctis1WProductID = 0x12b3;
   public static Arctis9ProductID = 0x12c2;
   public static Arctis7XProductID = 0x12d7;
+  public static Arctis7PPlusProductID = 0x2212;
+  public static ArctisNova7ProductID = 0x2202;
 
   constructor(
     name: string,
