62f02bb9cd4651951a9a2bc4ed67750a5c7e833a
Richard Race <1273965+richrace@users.noreply.github.com>
2023-05-22 18:35:17 +0100

    DRY for code climate

--- src/__tests__/use_cases/filter_known_headphones.test.ts
+++ src/__tests__/use_cases/filter_known_headphones.test.ts
@@ -5,9 +5,21 @@ import FilterKnownHeadphones from '../../use_cases/filter_known_headphones';
 describe('FilterKnownHeadphones', () => {
   describe('#execute', () => {
     let useCase: FilterKnownHeadphones;
+    let knownHeadphone: KnownHeadphone;
 
     beforeEach(() => {
       useCase = new FilterKnownHeadphones();
+
+      knownHeadphone = {
+        batteryPercentIdx: 3,
+        chargingStatusIdx: 4,
+        micStatusIdx: 5,
+        name: 'Arctis 7X',
+        path: 'Made up Arctis 7X@',
+        productId: 4823,
+        vendorId: 4152,
+        writeBytes: [6, 18],
+      };
     });
 
     it('will reject paths that are "undefined"', () => {
@@ -20,38 +32,22 @@ describe('FilterKnownHeadphones', () => {
 
     it('will return a matched headphone by path', () => {
       expect(useCase.execute([{ path: 'Made up Arctis 7X@' }] as SimpleHeadphone[])).toEqual([
-        {
-          batteryPercentIdx: 3,
-          chargingStatusIdx: 4,
-          micStatusIdx: 5,
-          name: 'Arctis 7X',
-          path: 'Made up Arctis 7X@',
-          productId: 4823,
-          vendorId: 4152,
-          writeBytes: [6, 18],
-        } as KnownHeadphone,
+        knownHeadphone,
       ]);
     });
 
     it('will return a matched headphone by a real path', () => {
+      const realPath =
+        'IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS06@14100000/USB2.0 Hub             @14100000/AppleUSB20Hub@14100000/AppleUSB20HubPort@14120000/SteelSeries Arctis 7X@14120000/SteelSeries Arctis 7X@3/AppleUserUSBHostHIDDevice';
+      knownHeadphone.path = realPath;
+
       expect(
         useCase.execute([
           {
-            path: 'IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS06@14100000/USB2.0 Hub             @14100000/AppleUSB20Hub@14100000/AppleUSB20HubPort@14120000/SteelSeries Arctis 7X@14120000/SteelSeries Arctis 7X@3/AppleUserUSBHostHIDDevice',
+            path: realPath,
           },
         ] as SimpleHeadphone[])
-      ).toEqual([
-        {
-          batteryPercentIdx: 3,
-          chargingStatusIdx: 4,
-          micStatusIdx: 5,
-          name: 'Arctis 7X',
-          path: 'IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS06@14100000/USB2.0 Hub             @14100000/AppleUSB20Hub@14100000/AppleUSB20HubPort@14120000/SteelSeries Arctis 7X@14120000/SteelSeries Arctis 7X@3/AppleUserUSBHostHIDDevice',
-          productId: 4823,
-          vendorId: 4152,
-          writeBytes: [6, 18],
-        } as KnownHeadphone,
-      ]);
+      ).toEqual([knownHeadphone]);
     });
   });
 });
