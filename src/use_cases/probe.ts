dd3683190ea4773aba15ae196bfee4c28c085611
Richard Race <1273965+richrace@users.noreply.github.com>
2024-07-28 11:08:07 +0100

    Move output logging to the Probe class

--- src/use_cases/probe.ts
+++ src/use_cases/probe.ts
@@ -7,21 +7,40 @@ import Host from '../utils/host';
 export default class Probe {
   readonly devices: Device[];
 
-  constructor() {
+  constructor(private output: boolean = false) {
     this.devices = HID.devices().filter((d) => d.vendorId === KnownHeadphone.ArctisVendorID);
   }
 
-  testUnknownHeadset() {
-    // Order in popularity of the most used write bytes. We can get a false positive.
-    const knownBytes = [
-      [0x00, 0xb0],
-      [0x06, 0x18],
-      [0x06, 0x12],
-      [0x0, 0x20],
-      [0x40, 0xaa],
-    ];
+  // Order in popularity of the most used write bytes. We can get a false positive.
+  static knownBytes = [
+    [0x00, 0xb0],
+    [0x06, 0x18],
+    [0x06, 0x12],
+    [0x06, 0xb0],
+    [0x0, 0x20],
+    [0x40, 0xaa]
+  ];
 
-    const foundHeadphones = this.devices.map((device) => {
+  testUnknownHeadset(): ProbeResult[] {
+    const foundHeadphones = this.findHeadphones();
+
+    const filteredFoundHeadphones = foundHeadphones.filter((result: ProbeResult) => {
+      if (result.matchedReport === undefined) {
+        return false;
+      }
+
+      return result.matchedReport.length > 0;
+    });
+
+    if (this.output) {
+      this.outputMessage(filteredFoundHeadphones);
+    }
+
+    return filteredFoundHeadphones;
+  }
+
+  private findHeadphones(): ProbeResult[] {
+    return this.devices.map((device) => {
       console.log('Probing', device.product);
 
       const devicePath = device.path;
@@ -36,7 +55,7 @@ export default class Probe {
             deviceProductId: device.productId,
             matchedBytes: undefined,
             matchedReport: undefined,
-            devicePath,
+            devicePath
           } as ProbeResult;
         }
         try {
@@ -48,7 +67,7 @@ export default class Probe {
             deviceProductId: device.productId,
             matchedBytes: undefined,
             matchedReport: undefined,
-            devicePath,
+            devicePath
           } as ProbeResult;
         }
       } else {
@@ -61,7 +80,7 @@ export default class Probe {
             deviceProductId: device.productId,
             matchedBytes: undefined,
             matchedReport: undefined,
-            devicePath,
+            devicePath
           } as ProbeResult;
         }
       }
@@ -70,7 +89,7 @@ export default class Probe {
       let matchedBytes;
       let matchedReport;
 
-      knownBytes.some((bytes) => {
+      Probe.knownBytes.some((bytes) => {
         console.log('\tWith', bytes);
         try {
           hidDevice.write(bytes);
@@ -79,12 +98,14 @@ export default class Probe {
           if (report.length > 0) {
             matchedBytes = bytes;
             matchedReport = report;
-            console.log('\tSuccess!');
+            console.log('\t\tSuccess!');
 
             return true;
           }
+
+          console.log("\t\tCouldn't get report");
         } catch {
-          // skip
+          console.log("\t\tCouldn't connect");
         }
       });
 
@@ -93,16 +114,37 @@ export default class Probe {
         deviceProductId: device.productId,
         matchedBytes,
         matchedReport,
-        devicePath,
+        devicePath
       } as ProbeResult;
     });
+  }
 
-    return foundHeadphones.filter((result: ProbeResult) => {
-      if (result.matchedReport === undefined) {
-        return false;
-      }
+  private outputMessage(probeResults: ProbeResult[]): void {
+    console.log(' ');
+    console.log(' ');
 
-      return result.matchedReport.length > 0;
+    if (probeResults.length === 0) {
+      console.log('No devices found');
+      return;
+    }
+
+    console.log('Use can use the following information to create a develop your own solution and create a PR.');
+    console.log('Or please copy / paste the following information and create an issue on the GitHub repo.');
+    console.log('Go to: https://github.com/richrace/arctis-usb-finder/');
+    console.log('You can look for values changing when press mute/changing chat mixer/charging');
+    console.log(' ');
+    console.log(' ');
+    console.log('Devices found:');
+    console.log(' ');
+
+    probeResults.forEach((result: ProbeResult) => {
+      console.log(' ');
+      console.log('Product:', result.deviceProductName);
+      console.log('Product ID:', result.deviceProductId);
+      console.log('Bytes:', result.matchedBytes);
+      console.log('Report:', result.matchedReport);
+      console.log('Path:', result.devicePath);
+      console.log(' ');
     });
   }
 }
